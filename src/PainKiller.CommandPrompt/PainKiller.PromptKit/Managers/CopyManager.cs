a5001358add860a9379d0f58f874d4dcbbb0dc4e
Harri Klingsten <harri.klingsten2@scb.se>
2025-03-31 00:24:33 +0200

    Implemented first version of PromptKit project creation.

--- src/PainKiller.CommandPrompt/PainKiller.PromptKit/Managers/CopyManager.cs
+++ src/PainKiller.CommandPrompt/PainKiller.PromptKit/Managers/CopyManager.cs
@@ -59,17 +59,21 @@ public class CopyManager(TemplatePaths paths)
     {
         _logger.LogDebug($"Copy files from root directory {paths.ApplicationRoot}");
         Environment.CurrentDirectory = paths.ApplicationRoot.Source;
-        File.Copy($"{nameof(Program)}.cs", Path.Combine(paths.ApplicationRoot.Target, $"{nameof(Program)}.cs"));
-        _logger.LogDebug($"Copy {nameof(Program)}.cs");
-        File.Copy("PainKiller.PromptKit.csproj", Path.Combine(paths.ApplicationRoot.Target, $"{projectName}.csproj"));
+
+        //Program.cs
+        var programContent = ReadFileAndReplace($"{nameof(Program)}.cs", "PainKiller.PromptKit", projectName);
+        File.WriteAllText(Path.Combine(paths.ApplicationRoot.Target, $"{nameof(Program)}.cs"), programContent);
+
+        File.Copy("PainKiller.PromptKit.csproj", Path.Combine(paths.ApplicationRoot.Target, $"{projectName}Client.csproj"));
         _logger.LogDebug($"Copy PainKiller.PromptKit.csproj to {projectName}.csproj");
+        
         Directory.CreateDirectory(Path.Combine(paths.ApplicationRoot.Target, nameof(Bootstrap)));
         _logger.LogDebug($"Create directory {Path.Combine(paths.ApplicationRoot.Target, nameof(Bootstrap))}");
         var commandPromptConfigurationContent = @"using PainKiller.CommandPrompt.CoreLib.Configuration.DomainObjects;
 namespace PainKiller.PromptKit.Bootstrap;
 public class CommandPromptConfiguration : ApplicationConfiguration
 {
-    public PromptKitConfiguration Custom { get; set; } = new();
+    public CustomConfiguration Custom { get; set; } = new();
 }".Replace("PainKiller.PromptKit", projectName);
         File.WriteAllText(Path.Combine(paths.ApplicationRoot.Target, nameof(Bootstrap), $"{nameof(CommandPromptConfiguration)}.cs"), commandPromptConfigurationContent);
         _logger.LogDebug($"Create {Path.Combine(paths.ApplicationRoot.Target, nameof(Bootstrap), $"{nameof(CommandPromptConfiguration)}.cs")}");
@@ -80,10 +84,14 @@ public class CustomConfiguration
 }".Replace("PainKiller.PromptKit", projectName);
         File.WriteAllText(Path.Combine(paths.ApplicationRoot.Target, nameof(Bootstrap), "CustomConfiguration.cs"), customConfigurationContent);
         _logger.LogDebug($"Create {Path.Combine(paths.ApplicationRoot.Target, nameof(Bootstrap), "CustomConfiguration.cs")}");
+
+        var startupContent = ReadFileAndReplace(Path.Combine($"{nameof(Bootstrap)}", $"{nameof(Startup)}.cs"), "PainKiller.PromptKit", projectName);
+        File.WriteAllText(Path.Combine(paths.ApplicationRoot.Target, nameof(Bootstrap), $"{nameof(Startup)}.cs"), startupContent);
         
         Directory.CreateDirectory(Path.Combine(paths.ApplicationRoot.Target, nameof(Commands)));
-        File.Copy(Path.Combine(nameof(Commands),$"{nameof(DemoCommand)}.cs"), Path.Combine(paths.ApplicationRoot.Target, nameof(Commands),$"{nameof(DemoCommand)}.cs"));
-        _logger.LogDebug($"Create {Path.Combine(paths.ApplicationRoot.Target, nameof(Commands),$"{nameof(DemoCommand)}.cs")}");
+
+        var demoCommandContent = ReadFileAndReplace(Path.Combine(nameof(Commands), $"{nameof(DemoCommand)}.cs"), "PainKiller.PromptKit", projectName);
+        File.WriteAllText(Path.Combine(paths.ApplicationRoot.Target, nameof(Commands),$"{nameof(DemoCommand)}.cs"), demoCommandContent);
     }
     public void CreateSolutionFile(string projectName)
     {
@@ -93,5 +101,12 @@ public class CustomConfiguration
         var targetSlnFilePath = Path.Combine(Directory.GetParent(paths.ApplicationRoot.Target)!.FullName, $"{projectName}.sln");
         File.WriteAllText(targetSlnFilePath, content);
         _logger.LogDebug($"Create {targetSlnFilePath}");
+    }
+
+    public string ReadFileAndReplace(string fileName, string find, string replace)
+    {
+        var content = File.ReadAllText(fileName);
+        var retVal = content.Replace(find, replace);
+        return retVal;
     }
 }
