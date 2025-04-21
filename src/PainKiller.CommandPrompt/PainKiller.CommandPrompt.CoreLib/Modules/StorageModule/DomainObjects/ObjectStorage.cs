2576209213c37d19f693324f93b9e55aa1331553
Harri Klingsten <harri.klingsten2@scb.se>
2025-04-21 10:56:59 +0200

    Adjusted access level

--- src/PainKiller.CommandPrompt/PainKiller.CommandPrompt.CoreLib/Modules/StorageModule/DomainObjects/ObjectStorage.cs
+++ src/PainKiller.CommandPrompt/PainKiller.CommandPrompt.CoreLib/Modules/StorageModule/DomainObjects/ObjectStorage.cs
@@ -3,36 +3,36 @@ using PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.Services;
 namespace PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.DomainObjects;
 public class ObjectStorage<T, TItem> : IObjectStorage<T, TItem> where T : IDataObjects<TItem>, new() where TItem : class, new()
 {
-    private T _dataObject = StorageService<T>.Service.GetObject();
+    protected T DataObject = StorageService<T>.Service.GetObject();
 
     public virtual List<TItem> GetItems(bool reload = false)
     {
-        if (reload) _dataObject = StorageService<T>.Service.GetObject();
-        return _dataObject.Items;
+        if (reload) DataObject = StorageService<T>.Service.GetObject();
+        return DataObject.Items;
     }
     public virtual void SaveItems(List<TItem> items)
     {
-        _dataObject.Items = items;
-        _dataObject.LastUpdated = DateTime.Now;
-        StorageService<T>.Service.StoreObject(_dataObject);
+        DataObject.Items = items;
+        DataObject.LastUpdated = DateTime.Now;
+        StorageService<T>.Service.StoreObject(DataObject);
     }
     public virtual void Insert(TItem item, Func<TItem, bool> match, bool saveToFile = true)
     {
-        var existing = _dataObject.Items.FirstOrDefault(match);
-        if (existing != null) _dataObject.Items.Remove(existing);
-        _dataObject.Items.Add(item);
-        _dataObject.LastUpdated = DateTime.Now;
-        if(saveToFile) StorageService<T>.Service.StoreObject(_dataObject);
+        var existing = DataObject.Items.FirstOrDefault(match);
+        if (existing != null) DataObject.Items.Remove(existing);
+        DataObject.Items.Add(item);
+        DataObject.LastUpdated = DateTime.Now;
+        if(saveToFile) StorageService<T>.Service.StoreObject(DataObject);
     }
     public virtual bool Remove(Func<TItem, bool> match, bool saveToFile = true)
     {
-        var existing = _dataObject.Items.FirstOrDefault(match);
+        var existing = DataObject.Items.FirstOrDefault(match);
         if (existing == null) return false;
 
-        _dataObject.Items.Remove(existing);
-        _dataObject.LastUpdated = DateTime.Now;
-        StorageService<T>.Service.StoreObject(_dataObject);
+        DataObject.Items.Remove(existing);
+        DataObject.LastUpdated = DateTime.Now;
+        StorageService<T>.Service.StoreObject(DataObject);
         return true;
     }
-    public virtual void Save() => StorageService<T>.Service.StoreObject(_dataObject);
+    public virtual void Save() => StorageService<T>.Service.StoreObject(DataObject);
 }
