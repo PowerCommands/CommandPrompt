eae4f365cdb562aa2fb0b23df057a1ffb275853c
Harri Klingsten <harri.klingsten2@scb.se>
2025-04-21 10:52:34 +0200

    Added the IDataObject and IObjectStorage.

--- src/PainKiller.CommandPrompt/PainKiller.CommandPrompt.CoreLib/Modules/StorageModule/Commands/StorageCommand.cs
+++ src/PainKiller.CommandPrompt/PainKiller.CommandPrompt.CoreLib/Modules/StorageModule/Commands/StorageCommand.cs
@@ -3,6 +3,7 @@ using PainKiller.CommandPrompt.CoreLib.Core.Enums;
 using PainKiller.CommandPrompt.CoreLib.Core.Events;
 using PainKiller.CommandPrompt.CoreLib.Core.Extensions;
 using PainKiller.CommandPrompt.CoreLib.Metadata.Attributes;
+using PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.DomainObjects;
 using PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.Services;
 
 namespace PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.Commands;
@@ -13,8 +14,8 @@ public class StorageCommand(string identifier) : ConsoleCommandBase<ApplicationC
 {
     public override void OnInitialized()
     {
-        var dir = StorageService<Description>.Service.GetRootDirectory();
-        var backupDir = StorageService<Description>.Service.GetBackupDirectory();
+        var dir = StorageService<Customer>.Service.GetRootDirectory();
+        var backupDir = StorageService<Customer>.Service.GetBackupDirectory();
         if (dir.Exists && backupDir.Exists) return;
         InitDirectories(dir.FullName, backupDir.FullName);
     }
@@ -23,16 +24,25 @@ public class StorageCommand(string identifier) : ConsoleCommandBase<ApplicationC
         Directory.CreateDirectory(rootDir);
         Directory.CreateDirectory(backupDir);
     }
-
     public override RunResult Run(ICommandLineInput input)
     {
-        var dir = StorageService<Description>.Service.GetRootDirectory();
+        var dir = StorageService<Customer>.Service.GetRootDirectory();
         Writer.WriteHeadLine($"{Emo.Directory.Icon()} App directory {dir.FullName} {dir.GetDirectorySize().GetDisplayFormattedFileSize()}");
         foreach (var file in dir.GetFiles()) Writer.WriteLine($"├──{Emo.File.Icon()} {file.Name}");
         Environment.CurrentDirectory = dir.FullName;
         EventBusService.Service.Publish(new WorkingDirectoryChangedEventArgs(Environment.CurrentDirectory));
+
+        //Simple way of adding items
+        var customers = new Customers{Items = [new Customer(), new Customer{Name = "Johnny Logan"}] };
+        StorageService<Customers>.Service.StoreObject(customers);
+
+        //More "advanced way" with Crud operations
+        var customerStorage = new ObjectStorage<Customers, Customer>();
+        var customer = new Customer { Name = "Helena Thompson" };
+        customerStorage.Insert(customer, c => c.Id == customer.Id);
+
+        var storedCustomers = customerStorage.GetItems();
+        Writer.WriteTable(storedCustomers);
         return Ok();
     }
-
-    private class Description { private string Name { get; set; } = "Sven Gurra Aktersnurra"; }
 }
---
+++ src/PainKiller.CommandPrompt/PainKiller.CommandPrompt.CoreLib/Modules/StorageModule/DomainObjects/ObjectStorage.cs
@@ -0,0 +1,38 @@
+﻿using PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.Contracts;
+using PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.Services;
+namespace PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.DomainObjects;
+public class ObjectStorage<T, TItem> : IObjectStorage<T, TItem> where T : IDataObjects<TItem>, new() where TItem : class, new()
+{
+    private T _dataObject = StorageService<T>.Service.GetObject();
+
+    public virtual List<TItem> GetItems(bool reload = false)
+    {
+        if (reload) _dataObject = StorageService<T>.Service.GetObject();
+        return _dataObject.Items;
+    }
+    public virtual void SaveItems(List<TItem> items)
+    {
+        _dataObject.Items = items;
+        _dataObject.LastUpdated = DateTime.Now;
+        StorageService<T>.Service.StoreObject(_dataObject);
+    }
+    public virtual void Insert(TItem item, Func<TItem, bool> match, bool saveToFile = true)
+    {
+        var existing = _dataObject.Items.FirstOrDefault(match);
+        if (existing != null) _dataObject.Items.Remove(existing);
+        _dataObject.Items.Add(item);
+        _dataObject.LastUpdated = DateTime.Now;
+        if(saveToFile) StorageService<T>.Service.StoreObject(_dataObject);
+    }
+    public virtual bool Remove(Func<TItem, bool> match, bool saveToFile = true)
+    {
+        var existing = _dataObject.Items.FirstOrDefault(match);
+        if (existing == null) return false;
+
+        _dataObject.Items.Remove(existing);
+        _dataObject.LastUpdated = DateTime.Now;
+        StorageService<T>.Service.StoreObject(_dataObject);
+        return true;
+    }
+    public virtual void Save() => StorageService<T>.Service.StoreObject(_dataObject);
+}
